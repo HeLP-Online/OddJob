0492e98bbda34744ec8535b7b75d2e837e5c2c7e
BuildTools <unconfigured@null.spigotmc.org>
2019-09-07 05:01:50 +0200

    guild names

---
+++ src/no/helponline/Commands/DeathCommand.java
@@ -0,0 +1,21 @@
+package no.helponline.Commands;
+
+import no.helponline.OddJob;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class DeathCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        int timeCheck = 0;
+        try {
+            timeCheck = Integer.parseInt(strings[0]);
+        } catch (Exception e) {
+            OddJob.getInstance().log("int error");
+        }
+        int i = OddJob.getInstance().getDeathManager().timeCheck(timeCheck);
+        OddJob.getInstance().getMessageManager().sendMessage(commandSender, "Replaced " + i + " chests");
+        return true;
+    }
+}
