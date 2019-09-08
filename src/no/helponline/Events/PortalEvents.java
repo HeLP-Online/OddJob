31ddcce3033f1abe9e438e21944e29e1110423f7
BuildTools <unconfigured@null.spigotmc.org>
2019-09-08 10:54:36 +0200

    fixin death

---
+++ src/no/helponline/Events/PortalEvents.java
@@ -0,0 +1,15 @@
+package no.helponline.Events;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerPortalEvent;
+
+public class PortalEvents implements Listener {
+    public void onEnterPortal(PlayerPortalEvent event) {
+        Player player = event.getPlayer();
+        Location locationFrom = event.getFrom();
+        Location locationTo = event.getTo();
+
+    }
+}
